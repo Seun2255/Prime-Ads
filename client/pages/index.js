4929e8187c6d323269ad8dc80cc6c37791f14faf
Primal <81525899+Seun2255@users.noreply.github.com>
2022-11-13 05:15:49 +0100

    quick fix

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,10 @@
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  router.push("/app/1");
+
   return <div className={styles.container}></div>;
 }
